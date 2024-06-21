7ce5e1fe332c66618df6f4b95077cc9b081e7f0e
j27000 <namanjhala32@gmail.com>
2024-06-21 09:01:51 -0400

    useeffect

--- Ebook/src/pages/Home/components/FeaturedProducts.js
+++ Ebook/src/pages/Home/components/FeaturedProducts.js
@@ -1,12 +1,29 @@
+import { useState, useEffect } from "react"
 import { ProductCard } from "../../../components/Elements/ProductCard"
+
 export const FeaturedProducts = () => {
+
+  const [task, settask] = useState([]);
+  useEffect(() => {
+    async function found() {
+      const data = await fetch("http://localhost:8000/featured_products");
+      const response = await data.json();
+      console.log(response);
+      settask(response);
+
+    }
+    found();
+
+
+  }, [])
   return (
     <section className="my-20">
       <h1 className="text-2xl text-center font-semibold text-black-100 mb-5 underline underline-offset-8">Featured eBooks</h1>
       <div className="flex flex-wrap justify-center lg:flex-row">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {task.map((pro) => (
+
+          <ProductCard key={pro.id} product ={pro} />
+        ))}
       </div>
     </section>
   )
