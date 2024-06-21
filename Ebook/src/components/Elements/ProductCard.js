7fac05b972afc0397c835ff3c49d8010f4363df1
j27000 <namanjhala32@gmail.com>
2024-06-21 09:00:39 -0400

    new productcard

--- Ebook/src/components/Elements/ProductCard.js
+++ Ebook/src/components/Elements/ProductCard.js
@@ -1,24 +1,24 @@
-import{Link} from "react-router-dom"
-export const ProductCard = () => {
+import { Link } from "react-router-dom"
+import { Stars } from "./Stars";
+export const ProductCard = ({ product }) => {
+    const { id, name, overview, poster, best_seller, rating } = product;
     return (
+
         <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <Link to="/" className="relative" >
-                <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">Best Seller</span>
-                <img className="rounded-t-lg w-full h-64" src="" alt="" />
+                {best_seller && <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">Best Seller</span>}
+
+                <img className="rounded-t-lg w-full h-64" src={poster} alt="" />
             </Link>
             <div className="p-5">
                 <Link to="/">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">The Complete Guide to Backend Development</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
                 </Link>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error unde quisquam magni vel eligendi nam.</p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
 
-                <div className="flex items-center my-2">
-                    <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                    <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                    <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                    <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                    <i className="text-lg bi bi-star text-yellow-500 mr-1"></i>
-                </div>
+                <Stars key={id} rating={rating} />
+
+
 
                 <p className="flex justify-between items-center">
                     <span className="text-2xl dark:text-gray-200">
