9c8044b0d51e5f3d78725e5aeae2fcf14218f7be
j27000 <namanjhala32@gmail.com>
2024-06-25 10:24:26 -0400

    detail page

--- Ebook/src/components/Elements/ProductCard.js
+++ Ebook/src/components/Elements/ProductCard.js
@@ -11,7 +11,7 @@ export const ProductCard = ({ product }) => {
                 <img className="rounded-t-lg w-full h-64" src={poster} alt="" />
             </Link>
             <div className="p-5">
-                <Link to="/">
+                <Link to={`${id}`}>
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
                 </Link>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
