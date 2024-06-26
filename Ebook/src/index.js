0d6a8b1899101b9a50ae58f89b3be0693cc6d389
j27000 <namanjhala32@gmail.com>
2024-06-26 13:54:27 -0400

    implement reducers and context

--- Ebook/src/index.js
+++ Ebook/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ProductProvider } from './Context';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ProductProvider>
+        <ToastContainer />
+        <App />
+
+      </ProductProvider>
 
     </Router>
   </React.StrictMode>
