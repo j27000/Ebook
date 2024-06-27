53398dcdea0072cd55cad42117be77927afa5375
j27000 <namanjhala32@gmail.com>
2024-06-27 12:41:14 -0400

    cartreducer

--- Ebook/src/routes/AllRoutes.js
+++ Ebook/src/routes/AllRoutes.js
@@ -1,14 +1,24 @@
 import { Route, Routes } from "react-router-dom"
 import { HomePage, ProductDetail } from "../pages"
 import { ProductsList } from "../pages"
+import { Login } from "../pages"
+import { Register } from "../pages"
+import { CartPage } from "../pages/Cart/CartPage"
+
+
 
 export const AllRoutes = () => {
     return (
 
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="ProductsList" element={<ProductsList />} />
-            <Route path="ProductsList/:id" element={<ProductDetail />} />
+            <Route path="/Products" element={<ProductsList />} />
+            <Route path="/Products/:id" element={<ProductDetail />} />
+            <Route path="/:id" element={<ProductDetail />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Cart" element={<CartPage/>} />
+
         </Routes>
     )
 }
