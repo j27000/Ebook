875146ec262df4ee5279a1b89d0f26abc5435f35
j27000 <namanjhala32@gmail.com>
2024-06-27 12:40:15 -0400

    cartreducer

--- Ebook/src/components/Elements/DropdownLoggedIn.js
+++ Ebook/src/components/Elements/DropdownLoggedIn.js
@@ -1,5 +1,11 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 export const DropdownLoggedIn = () => {
+    const navigate = useNavigate()
+    function handletoken() {
+        sessionStorage.removeItem("token");
+        sessionStorage.removeItem("nid");
+        navigate("/");
+    }
     return (
         <div id="dropdownAvatar" className="select-none	absolute top-10 right-0 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
             <div className="py-3 px-4 text-sm text-gray-900 dark:text-white">
@@ -14,7 +20,7 @@ export const DropdownLoggedIn = () => {
                 </li>
             </ul>
             <div className="py-1">
-                <span className="cursor-pointer block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log out</span>
+                <span onClick={handletoken} className="cursor-pointer block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log out</span>
             </div>
         </div>
     )
